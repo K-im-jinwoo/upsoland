45f8ba2c34626c0acf7ec7e5719820ff059ca2f5
Jinwoo Kim <zaqaz113@naver.com>
2022-11-26 14:44:20 +0900

    포스트 목록 페이지에 이미지 출력

--- mysite/post/models.py
+++ mysite/post/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Post(models.Model):
   title = models.CharField(max_length=30)
   content = models.TextField()
+  head_image = models.ImageField(upload_to='post/images/%Y/%m/%d/', blank=True)
   dt_create = models.DateTimeField(auto_now_add=True)
   dt_update = models.DateTimeField(auto_now=True)
 
